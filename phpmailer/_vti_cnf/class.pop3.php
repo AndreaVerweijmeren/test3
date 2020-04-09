2c2ce3a45c6774bac001c213315ae51d01f5b543
AndreaVerweijmeren <61025668+AndreaVerweijmeren@users.noreply.github.com>
2020-04-09 19:02:19 +0200

    test

---
+++ phpmailer/_vti_cnf/class.pop3.php
@@ -0,0 +1,17 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 Mar 2010 15:36:20 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_author:SR|MARCOSANDRA\\Marco en Sandra
+vti_modifiedby:SR|MARCOSANDRA\\Marco en Sandra
+vti_timecreated:TR|20 Oct 2009 00:48:22 -0000
+vti_backlinkinfo:VX|
+vti_syncofs_213.75.22.105\:21/wwwroot:TW|18 Dec 2012 09:36:00 -0000
+vti_syncwith_213.75.22.105\:21/wwwroot:TX|31 Mar 2010 15:36:20 -0000
+vti_nexttolasttimemodified:TR|31 Mar 2010 10:51:15 -0000
+vti_cacheddtm:TX|31 Mar 2010 15:36:20 -0000
+vti_filesize:IR|10599
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|windows-1252
